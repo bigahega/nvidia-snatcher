01fd5bfbca9f0d34fccda54ac2c98822a23ac076
Berkin Güler <berkinguler@live.com>
2021-01-25 00:59:01 +0300

    added 3080 amazon tr links

--- src/store/model/amazon-tr.ts
+++ src/store/model/amazon-tr.ts
@@ -22,6 +22,48 @@ export const AmazonTr: Store = {
 			url: 'https://www.amazon.com.tr/ASUS-GEFORCE-TUF-RTX3080-10G-GAMING-320bit-Express/dp/B08HN37VQK?ie=UTF8&language=tr_TR'
 		},
 
+		{
+			brand: 'msi',
+			model: 'suprim x',
+			series: '3080',
+			url: 'https://www.amazon.com.tr/GEFORCE-RTX-3080-SUPRIM-10G/dp/B08LTKLG5K/ref=sr_1_1?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-1'
+		},
+
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3080',
+			url: 'https://www.amazon.com.tr/Gigabyte-GeForce-Gaming-GV-N3080GAMING-OC-10GD/dp/B08HLZXHZY/ref=sr_1_2?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-2'
+		},
+
+		{
+			brand: 'msi',
+			model: 'ventus',
+			series: '3080',
+			url: 'https://www.amazon.com.tr/GeForce-3080-VENTUS-3X-10G/dp/B08HM4M621/ref=sr_1_3?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-3'
+		},
+
+		{
+			brand: 'msi',
+			model: 'x trio',
+			series: '3080',
+			url: 'https://www.amazon.com.tr/GeForce-3080-GAMING-TRIO-10G/dp/B08HM4V2DH/ref=sr_1_4?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-4'
+		},
+
+		{
+			brand: 'gigabyte',
+			model: 'eagle',
+			series: '3080',
+			url: 'https://www.amazon.com.tr/GIGABYTE-RTX3080-GV-N3080EAGLEOC10GD-GDDR6X-320bit/dp/B08HHZVZ3N/ref=sr_1_7?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-7'
+		},
+
+		{
+			brand: 'gigabyte',
+			model: 'aorus',
+			series: '3080',
+			url: 'https://www.amazon.com.tr/Gigabyte-AORUS-GeForce-Xtreme-GDDR6X/dp/B08M5ZFNTT/ref=sr_1_8?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-8'
+		},
+
 	],
 	name: 'amazon-tr'
 };
