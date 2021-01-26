b50b8329493ce0bdd33d62a890189c79c602b9a8
Berkin Güler <berkinguler@live.com>
2021-01-27 01:25:23 +0300

    Update amazon-tr.ts

--- src/store/model/amazon-tr.ts
+++ src/store/model/amazon-tr.ts
@@ -19,126 +19,133 @@ export const AmazonTr: Store = {
 			brand: 'asus',
 			model: 'tuf oc',
 			series: '3080',
-			url: 'https://www.amazon.com.tr/ASUS-GEFORCE-TUF-RTX3080-10G-GAMING-320bit-Express/dp/B08HN37VQK?ie=UTF8&language=tr_TR'
+			url: 'https://www.amazon.com.tr/dp/B08HN37VQK'
 		},
 
 		{
 			brand: 'msi',
 			model: 'suprim x',
 			series: '3080',
-			url: 'https://www.amazon.com.tr/GEFORCE-RTX-3080-SUPRIM-10G/dp/B08LTKLG5K/ref=sr_1_1?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-1'
+			url: 'https://www.amazon.com.tr/dp/B08LTKLG5K'
 		},
 
 		{
 			brand: 'gigabyte',
 			model: 'gaming oc',
 			series: '3080',
-			url: 'https://www.amazon.com.tr/Gigabyte-GeForce-Gaming-GV-N3080GAMING-OC-10GD/dp/B08HLZXHZY/ref=sr_1_2?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-2'
+			url: 'https://www.amazon.com.tr/dp/B08HLZXHZY'
 		},
 
 		{
 			brand: 'msi',
 			model: 'ventus',
 			series: '3080',
-			url: 'https://www.amazon.com.tr/GeForce-3080-VENTUS-3X-10G/dp/B08HM4M621/ref=sr_1_3?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-3'
+			url: 'https://www.amazon.com.tr/dp/B08HM4M621'
 		},
 
 		{
 			brand: 'msi',
 			model: 'x trio',
 			series: '3080',
-			url: 'https://www.amazon.com.tr/GeForce-3080-GAMING-TRIO-10G/dp/B08HM4V2DH/ref=sr_1_4?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-4'
+			url: 'https://www.amazon.com.tr/dp/B08HM4V2DH'
 		},
 
 		{
 			brand: 'gigabyte',
 			model: 'eagle',
 			series: '3080',
-			url: 'https://www.amazon.com.tr/GIGABYTE-RTX3080-GV-N3080EAGLEOC10GD-GDDR6X-320bit/dp/B08HHZVZ3N/ref=sr_1_7?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-7'
+			url: 'https://www.amazon.com.tr/dp/B08HHZVZ3N'
 		},
 
 		{
 			brand: 'gigabyte',
 			model: 'aorus',
 			series: '3080',
-			url: 'https://www.amazon.com.tr/Gigabyte-AORUS-GeForce-Xtreme-GDDR6X/dp/B08M5ZFNTT/ref=sr_1_8?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=3080&qid=1611495135&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-8'
+			url: 'https://www.amazon.com.tr/dp/B08M5ZFNTT'
 		},
 
 		{
 			brand: 'asus',
 			model: 'tuf oc',
 			series: '3070',
-			url: 'https://www.amazon.com.tr/GEFORCE-TUF-RTX3070-O8G-GAMING-GDDR6-256bit-2xHDMI/dp/B08LLG9KQT/ref=sr_1_12?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=rtx+3070&qid=1611570469&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-12'
+			url: 'https://www.amazon.com.tr/dp/B08LLG9KQT'
 		},
 
 		{
 			brand: 'gigabyte',
 			model: 'vision oc',
 			series: '3070',
-			url: 'https://www.amazon.com.tr/Gigabyte-GeForce-Vision-ekran-kartı/dp/B08LNWPYRS/ref=sr_1_1?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=rtx+3070&qid=1611570469&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-1'
+			url: 'https://www.amazon.com.tr/dp/B08LNWPYRS'
 		},
 
 		{
 			brand: 'msi',
 			model: 'ventus 3x',
 			series: '3070',
-			url: 'https://www.amazon.com.tr/RTX-3070-VENTUS-3X-OC/dp/B08L6PCZTR/ref=sr_1_2?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=rtx+3070&qid=1611570469&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-2'
+			url: 'https://www.amazon.com.tr/dp/B08L6PCZTR'
 		},
 
 		{
 			brand: 'zotac',
 			model: 'gaming twin edge',
 			series: '3070',
-			url: 'https://www.amazon.com.tr/ZOTAC-GAMING-GeForce-3070-Twin/dp/B08LBVNKT1/ref=sr_1_3?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=rtx+3070&qid=1611570469&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-3'
+			url: 'https://www.amazon.com.tr/dp/B08LBVNKT1'
 		},
 
 		{
 			brand: 'inno3d',
 			model: 'iChill',
 			series: '3070',
-			url: 'https://www.amazon.com.tr/INNO3D-GeForce-iChill-GDDR6-256-bit/dp/B08N4V68NX/ref=sr_1_4?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=rtx+3070&qid=1611570469&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-4'
+			url: 'https://www.amazon.com.tr/dp/B08N4V68NX'
 		},
 
 		{
 			brand: 'msi',
 			model: 'gaming x trio',
 			series: '3070',
-			url: 'https://www.amazon.com.tr/GeForce-RTX-3070-GAMING-TRIO/dp/B08LNPPCWJ/ref=sr_1_5?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=rtx+3070&qid=1611570469&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-5'
+			url: 'https://www.amazon.com.tr/dp/B08LNPPCWJ'
 		},
 
 		{
 			brand: 'zotac',
 			model: 'gaming twin edge white',
 			series: '3070',
-			url: 'https://www.amazon.com.tr/ZOTAC-Gaming-GEFORCE-White-konsolu/dp/B08P7ZKQPP/ref=sr_1_6?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=rtx+3070&qid=1611570469&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-6'
+			url: 'https://www.amazon.com.tr/dp/B08P7ZKQPP'
 		},
 
 		{
 			brand: 'gigabyte',
 			model: 'windforce',
 			series: '3070',
-			url: 'https://www.amazon.com.tr/GIGABYTE-GeForce-WINDFORCE-GV-N3070GAMING-OC-8GD/dp/B08KY266MG/ref=sr_1_7?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=rtx+3070&qid=1611570469&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-7'
+			url: 'https://www.amazon.com.tr/dp/B08KY266MG'
 		},
 
 		{
 			brand: 'gigabyte',
 			model: 'eagle',
 			series: '3070',
-			url: 'https://www.amazon.com.tr/Gigabyte-GeForce-EAGLE-ekran-kartı/dp/B08KHL2J5X/ref=sr_1_8?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=rtx+3070&qid=1611570469&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-8'
+			url: 'https://www.amazon.com.tr/dp/B08KHL2J5X'
 		},
 
 		{
 			brand: 'gigabyte',
 			model: 'aorus',
 			series: '3070',
-			url: 'https://www.amazon.com.tr/Gigabyte-AORUS-GeForce-Master-ekran/dp/B08LNY8P5L/ref=sr_1_9?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=rtx+3070&qid=1611570469&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-9'
+			url: 'https://www.amazon.com.tr/dp/B08LNY8P5L'
 		},
 
 		{
 			brand: 'msi',
 			model: 'suprim',
 			series: '3070',
-			url: 'https://www.amazon.com.tr/MSI-RTX-3070-SUPPRIM-8G/dp/B08QZP2RRS/ref=sr_1_10?__mk_tr_TR=ÅMÅŽÕÑ&dchild=1&keywords=rtx+3070&qid=1611570469&refinements=p_n_availability%3A13136583031&rnid=13136582031&s=computers&sr=1-10'
+			url: 'https://www.amazon.com.tr/dp/B08QZP2RRS'
+		},
+
+		{
+			brand: 'asus',
+			model: 'rog strix',
+			series: '3070',
+			url: 'https://www.amazon.com.tr/dp/B08KHHF881'
 		},
 
 	],
